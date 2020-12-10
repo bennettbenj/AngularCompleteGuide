bfed77db726c874bbe2d16b3c67d8254c30ee418
Ben <bb0139@my.bristol.ac.uk>
2020-12-10 11:59:50 +0000

    Completed assignment3

---
+++ assignment3/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'assignment3';
+  displayText = false;
+  clicks = [];
+  currentParagraph = 0;
+
+  onButtonClick(){
+    this.displayText = !this.displayText;
+    this.clicks.push("1");
+    this.currentParagraph++;
+  }
+
+  getColor(){
+    if(this.currentParagraph > 4) return 'blue'
+    else return null;
+  }
+}
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!'
   serverCreated = false;
   serverName = '';
+  servers = ['Testserver', 'Testserver2'];
 
   constructor() {
     setTimeout(() => {
@@ -25,6 +26,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = "Server was created!";
   }
 
