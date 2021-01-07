c90895781fe16f9a7aec3db9b751dd9bdff38e3b
Ben <bb0139@my.bristol.ac.uk>
2021-01-07 22:47:37 +0000

    Implemented recipes item component as its own component using property binding

--- recipeApp/src/app/header/header.component.ts
+++ recipeApp/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() headerButtonEvent = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  openRecipes(){
+    this.headerButtonEvent.emit(0);
+  }
+
+  openShoppingList(){
+    this.headerButtonEvent.emit(1);
+  }
 }
--- recipeApp/src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ recipeApp/src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipes-item',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-item.component.css']
 })
 export class RecipesItemComponent implements OnInit {
+
+  @Input() name;
+  @Input() description;
+  @Input() ImagePath;
 
   constructor() { }
 
