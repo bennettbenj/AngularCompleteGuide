8d8b1ec270331adaf9b8f00e0e550d1ac5a6fa6a
Ben Bennett <bb0139@my.bristol.ac.uk>
2021-01-11 14:35:44 +0000

    Added basic ingredient addition functionality

--- recipeApp/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipeApp/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
+import { ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput', {static: false}) nameInput: ElementRef;
+  @ViewChild('amountInput', {static: false}) amountInput: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addIngredient(){
+    const ingrName = this.nameInput.nativeElement.value;
+    const ingrAmount = this.amountInput.nativeElement.value;
+    const newIngredient = new Ingredient(ingrName, ingrAmount);
+    console.log(newIngredient);
+    
+    this.ingredientAdded.emit(newIngredient);
+    console.log("Add button clicked");
+    
+  }
+
+  deleteIngredient(){
+
+  }
+
+  clearIngredients(){
+
+  }
 }
--- recipeApp/src/app/shopping-list/shopping-list.component.ts
+++ recipeApp/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -8,7 +8,8 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
 
-  ingredients: Ingredient[] = [
+ 
+  @Input() ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ];
@@ -16,6 +17,12 @@ export class ShoppingListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addIngredient(newIngredient: Ingredient){
+    this.ingredients.push(newIngredient);
+    console.log("Ingredient added");
+    
   }
 
 }
